197a7cf9e77360c60d7a5d7462c86029aa94e2c6
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-06-15 23:32:31 -0300

    process.cpuUsage([previousValue])

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const process = require('process');
 
-console.log(process);
+var usage = process.cpuUsage()
+
+// CPU Usage value
+console.log(usage);
+
+// Printing the current time stamp
+const now = Date.now();
+
+// Looping to delay the process for 100 milliseconds
+while (Date.now() - now < 100);
+
+usage = process.cpuUsage(usage)
+
+// CPU Usage by this process
+console.log(usage);
